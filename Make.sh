29679e9affd01a5b7cf85a55ba280d2ac3333a76
Nigel Bray <nrbray@gmx.net>
2018-04-28 15:18:37 +0100

    syncthing v0.14.46; new config.xml; upload and check

--- Make.sh
+++ Make.sh
@@ -1,8 +1,12 @@
-cp /home/nrb/projects/kobo/.config/syncthing-kobo/config.xml /home/nrb/projects/xcsoar/ShareAllTasks/KoboRoot/.config/syncthing-kobo/config.xml
+cp config.xml KoboRoot/.config/syncthing-kobo/config.xml
 rm syncthing-linux-arm-v*.*.*.tar.gz*
-wget https://github.com/syncthing/syncthing/releases/download/v0.14.32/syncthing-linux-arm-v0.14.32.tar.gz; 
-tar xzvf syncthing-linux-arm-v0.14.32.tar.gz --strip-components 1 -C KoboRoot/bin/ syncthing-linux-arm-v0.14.32/syncthing; 
+wget https://github.com/syncthing/syncthing/releases/download/v0.14.46/syncthing-linux-arm-v0.14.46.tar.gz; 
+tar xzvf syncthing-linux-arm-v0.14.46.tar.gz --strip-components 1 -C KoboRoot/bin/ syncthing-linux-arm-v0.14.46/syncthing; 
 tree -a -I '.git' --matchdirs; 
-tar -cvzf Website/KoboRoot.tgz -C KoboRoot .
+mkdir -p Website;
+tar -cvzf Website/KoboRoot.tgz -C KoboRoot .;
 sha1sum Website/KoboRoot.tgz; 
 # rm KoboRoot/bin/syncthing KoboRoot.tgz
+scp Website/KoboRoot.tgz nrb@51.255.41.162:KoboRoot.tgz; 
+wget http://51.255.41.162/KoboRoot.tgz; 
+sha1sum KoboRoot.tgz; 
